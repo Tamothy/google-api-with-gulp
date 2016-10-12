b3ef23bdc47cd8c7a1de0c3f57fe87a565470897
Gray Hildreth and Tam Nguyen <mr.tamron@gmail.com>
2016-10-12 15:59:40 -0700

    add jQuery functionality

--- js/burrito.js
+++ js/burrito.js
@@ -1,37 +1,87 @@
+// window.onload = function (){
+//      var mapDiv = document.getElementById('map');
+//      var pos;
+//      $.get( "http://ip-api.com/json", function(data) {
+//        pos = new google.maps.LatLng(data.lat, data.lon);
+//        initMap();
+//      });
+//
+//      function initMap() {
+//         var map = new google.maps.Map(mapDiv, {
+//           zoom: 10,
+//           center: pos
+//
+//         });
+//      }
+//    }
+
 var map;
+var infowindow;
+
+var request;
+var service;
+var markers =[];
+
 function initialize() {
-  var center = new google.maps.LatLng(45.5231,-122.6765);
+  var curLocation = new google.maps.LatLng(45.5231,-122.6765);
   map = new google.maps.Map(document.getElementById('map'), {
-      center: center,
-      zoom:15,
-      mapTypeId: 'roadmap'
+      center: curLocation,
+      zoom:13,
+      // mapTypeId: 'roadmap'
   });
 
-  var request = {
-    location: center,
-    radius: 9000,
-    types: ['burrito']
+  request = {
+    location: curLocation,
+    radius: 6000,
+    keyword: 'burrito'
   };
+  infowindow = new google.maps.InfoWindow();
 
-  var service = new google.maps.places.PlacesService(map);
+  service = new google.maps.places.PlacesService(map);
 
   service.nearbySearch(request, callback);
+
+  google.maps.event.addListener(map, 'rightclick', function(event) {
+    map.setCenter(event.latLng)
+    clearResults(markers)
+
+    var request = {
+      location: event.latLng,
+      radius: 8000,
+      keyword: 'burrito'
+    };
+    service.nearbySearch(request, callback);
+  })
 }
 
 function callback(results, status) {
   if(status == google.maps.places.PlacesServiceStatus.OK) {
     for(var i = 0; i < results.length; i++) {
-      createMarker(results[i]);
+      markers.push(createMarker(results[i]));
     }
   }
 }
 
 function createMarker(place) {
   var placeLoc = place.geometry.location;
-  var marker = new google.maps.Marker({
+  marker = new google.maps.Marker({
     map: map,
     position: place.geometry.location
   });
+
+  google.maps.event.addListener(marker, 'click', function() {
+    infowindow.setContent(place.name);
+    infowindow.open(map, this);
+  });
+  return marker;
+
+}
+
+function clearResults(markers) {
+  for (var m in markers) {
+    markers[m].setMap(null)
+  }
+  markers = []
 }
 
 // google.maps.event.addDomListener(window, 'load', initialize);
